c03cad61039c4e9781d75dd6696bc332b9195c6c
Saeed <saeedmoshtari01@gmail.com>
2023-11-18 16:19:30 +0330

    feature/sm-6 add models to admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product,Image,Variety
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'brand', 'category', 'feature')
+
+
+class VarietyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'color')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Variety, VarietyAdmin)
+admin.site.register(Image)
